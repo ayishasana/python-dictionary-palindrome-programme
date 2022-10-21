710bb8f3bed1a743fcccbe4042e78258ab728036
ayishasana <sanaayisha1234@gmail.com>
2022-10-21 11:59:35 +0530

    pycharm

---
+++ palindrome.py
@@ -0,0 +1,5 @@
+string=str(input("enter the string:"))
+if (string==string[::-1]):
+    print("this is palindrome:")
+else:
+    print("this is not palindrome:")
---
+++ pythondictionary.py
@@ -0,0 +1,49 @@
+thisdict = {
+  "brand": "apple",
+  "model": "14 pro",
+  "year": 2022
+}
+print(thisdict["brand"])
+
+thisdict = {
+  "brand": "apple",
+  "model": "14 pro",
+  "year": 1964,
+  "year": 2022
+}
+print(thisdict)
+
+print(len(thisdict))
+thisdict = {
+  "brand": "apple",
+  "model": "14 pro",
+  "year": 2022
+}
+print(type(thisdict))
+
+
+thisdict = {
+  "brand": "apple",
+  "model": "14 pro",
+  "year": 2022
+}
+x = thisdict["model"]
+print(x)
+
+thisdict = {
+  "brand": "apple",
+  "model": "14 pro",
+  "year": 2022
+}
+x = thisdict.keys()
+print(x)
+
+phone = {
+"brand": "apple",
+"model": "14 pro",
+"year": 2022
+}
+x = phone.keys()
+print(x)
+phone["color"]="white"
+print(x)
